458e282494447ed707097ecf00e390ba1659f489
Ghiurutan-Bura Péter <50810629+gburapeter@users.noreply.github.com>
2023-10-05 16:33:18 +0200

    Update _index.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -5,8 +5,9 @@ import {
 	useTransform,
 	motion,
 	useSpring,
+	useAnimation,
 } from "framer-motion";
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
 import { AppleIcon } from "~/components/AppleIcon";
 import Cardgang from "~/components/Cardgang";
 import { Example } from "~/components/Example";
@@ -53,6 +54,17 @@ export default function Index() {
 
 	const isIphone2 = useInView(iphone2);
 
+	const macbookos = useRef(null);
+	const isMac = useInView(macbookos);
+
+	const mainControls = useAnimation();
+
+	useEffect(() => {
+		if (isMac) {
+			mainControls.start("visible");
+		}
+	}, [isMac]);
+
 	return (
 		<>
 			<MyNavbar />
@@ -60,21 +72,19 @@ export default function Index() {
 				<AppleIcon />
 			</Section>
 			<Section className="bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-black via-blue-800 to-black grid place-items-center !h-screen ">
-				<section
+				<motion.div
+					variants={{
+						hidden: { opacity: 0, x: 200 },
+						visible: { opacity: 1, x: 0 },
+					}}
+					initial="hidden"
+					animate={mainControls}
+					transition={{ duration: 1.5, delay: 0.5 }}
+					ref={macbookos}
 					className="mt-[20.5vh] text-5xl sm:text-6xl md:text-6xl lg:text-6xl xl:text-7xl 2xl:text-8xl"
-					ref={ref}
 				>
-					<span
-						style={{
-							transform: isInView ? "none" : "translateX(-200px)",
-							opacity: isInView ? 1 : 0,
-							transition:
-								"transform 2s ease-in-out, opacity 2s ease-in-out",
-						}}
-					>
-						Macbook Pro
-					</span>
-				</section>
+					Macbook Pro
+				</motion.div>
 				<Macbook />
 			</Section>
 			<Section className="bg-[radial-gradient(ellipse_at_bottom_left,_var(--tw-gradient-stops))] from-black via-blue-800 to-black grid !h-min pt-[45vh]">
@@ -152,8 +162,8 @@ export default function Index() {
 									xmlns="http://www.w3.org/2000/svg"
 								>
 									<path
-										fill-rule="evenodd"
-										clip-rule="evenodd"
+										fillRule="evenodd"
+										clipRule="evenodd"
 										d="M21.5 11.0631C21.5 5.26467 16.7984 0.56311 11 0.56311C5.20156 0.56311 0.5 5.26467 0.5 11.0631C0.5 16.3037 4.33906 20.6476 9.35938 21.4361V14.0992H6.69266V11.0631H9.35938V8.74983C9.35938 6.11873 10.9273 4.6642 13.3255 4.6642C14.4744 4.6642 15.6763 4.86952 15.6763 4.86952V7.45373H14.3516C13.048 7.45373 12.6402 8.2628 12.6402 9.09436V11.0631H15.552L15.087 14.0992H12.6406V21.437C17.6609 20.649 21.5 16.3051 21.5 11.0631Z"
 										fill="currentColor"
 									/>
